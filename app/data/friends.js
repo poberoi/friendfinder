d94e1ed0b778cc6be0a54cb70e0a62169e272458
Puneet Oberoi <puneet.oberoi@outlook.com>
2016-07-30 19:06:38 -0400

    More Friends added

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,21 +4,25 @@
 
 var friends = [
 {
-  name: "Ahmed",
-  photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  scores: [
-     5,
-     1,
-     4,
-     4,
-     5,
-     1,
-     2,
-     5,
-     4,
-     1
-   ]
-}
+  name: "Steven Seagal",
+  photo: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Steven_Seagal_by_Gage_Skidmore.jpg",
+  scores: [ 5, 1, 4, 4, 5, 1, 2, 5, 4, 1 ]
+},
+{
+  name: "Katy Perry",
+  photo: "https://upload.wikimedia.org/wikipedia/commons/7/77/Katy_Perry_3_November_2014.jpg",
+  scores: [ 1, 5, 3, 4, 2, 5, 1, 3, 2, 4 ]
+},
+{
+  name: "John Cena",
+  photo: "https://upload.wikimedia.org/wikipedia/commons/e/ea/John_Cena_as_WHC.jpg",
+  scores: [ 1, 5, 2, 4, 4, 5, 5, 1, 5, 3 ]
+},
+{
+  name: "Dwayne Johnson",
+  photo: "https://en.wikipedia.org/wiki/Dwayne_Johnson#/media/File:Dwayne_Johnson_Hercules_2014_(cropped).jpg",
+  scores: [ 1, 3, 2, 3, 2, 4, 3, 4, 3, 1 ]
+},
 ];
 
 module.exports = friends; 
